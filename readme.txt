3dd52fe5194ff20f0b136645a970e3d99a67e30a
Daniel Espinoza <daniel@growdevelopment.com>
2015-01-16 19:38:50 -0600

    Initial checkin

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== WooCommerce Template Hints ===
+Contributors: shopplugins, growdev
+Donate link: http://www.shopplugins.com
+Tags: woocommerce, ecommerce, woocommerce theme, woocommerce helper
+Requires at least: 3.6
+Tested up to: 4.1
+Stable tag: 1.0.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+WooCommerce Template Hints adds a visual border around parts of all WooCommerce pages showing details about the template used.
+
+== Description ==
+WooCommerce Template Hints adds a visual border around parts of all WooCommerce pages showing details about the template used.
+
+This is very helpful when developing a new theme for WooCommerce, or troubleshooting an existing theme.
+
+The code is maintained on <a href="https://github.com/growdev/woocommerce-template-hints">Github</a>
+
+**Translations, feature requests, and ratings are welcome and appreciated!**
+
+== Installation ==
+
+1. Upload the folder `woocommerce-template-hints` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to WooCommerce > System Status
+4. Click on the "Tools" tab
+5. Check the box next to Template Hint and save settings
+
+== Frequently Asked Questions ==
+
+= Will this show template overrides in my theme? =
+
+Yes! Whichever template is used on the site will be shown with a path.
+
+
+== Upgrade Notice ==
+
+None.
+
+== Screenshots ==
+
+1. Product page with template hints on
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
---
+++ woocommerce-template-hints.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: WooCommerce Template Hints
+Plugin URI: https://github.com/growdev/woocommerce-template-hints/
+Description: WooCommerce Template Hints adds a visual border around parts of all WooCommerce pages showing details about the template used.
+Version: 1.0.0
+Author: Shop Plugins
+Author URI: http://shopplugins.com
+Text Domain: woocommerce-template-hints
+
+ * Copyright Shop Plugins
+ *
+ *     This file is part of WooCommerce Availability Chart,
+ *     a plugin for WordPress.
+ *
+ *     WooCommerce Template Hints is free software:
+ *     You can redistribute it and/or modify it under the terms of the
+ *     GNU General Public License as published by the Free Software
+ *     Foundation, either version 3 of the License, or (at your option)
+ *     any later version.
+ *
+ *     WooCommerce Template Hints is distributed in the hope that
+ *     it will be useful, but WITHOUT ANY WARRANTY; without even the
+ *     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with WordPress. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+
+add_action('woocommerce_before_template_part', 'wcth_before_template',30, 4);
+function wcth_before_template( $template_name, $template_path, $located, $args ) {
+
+	echo '<fieldset style="border: 1px red solid; padding: 3px; ">';
+    echo "<legend style='padding: 1px; background: red; color: white; font-size: 10px;'>template: " . $template_name . " </legend>";
+}
+
+add_action('woocommerce_after_template_part', 'wcth_after_template',30, 4);
+function wcth_after_template( $template_name, $template_path, $located, $args ) {
+	echo "</fieldset>";
+}
+
